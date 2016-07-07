f66a89906f000ed3c7aa67ba3e5631b1104e1b14
march-dave <ysangy@gmail.com>
2016-07-06 22:47:47 -0700

    up to date

--- routes/rentals.js
+++ routes/rentals.js
@@ -2,31 +2,25 @@
 
 const express = require('express');
 const router = express.Router();
+const request = require('request');
 
 router.get('/', (req, res) => {
 
-    // var options = { method: 'GET',
-    //   url: 'https://zilyo.p.mashape.com/search',
-    //   qs: { swlatitude: '18.55', swlongitude: '-160.52999999999997' },
-    //   headers:
-    //    { provider: 'airbnb',
-    //      nelongitude: '154.48000000000002',
-    //      nelatitude: '22.37',
-    //      isinstantbook: 'true',
-    //      'x-mashape-key': 'TIjgWU0XSrmsh0zxEqbXqvmwRoytp17KCCtjsnjGAbKORMhWbX' },
-    //   form: { firstName: 'john', lastName: 'doe' } };
-    //
-    // request(options, function (error, response, body) {
-    //   if (error) throw new Error(error);
-    //
-    //   console.log(body);
-    // });
+    var options = { method: 'GET',
+      url: 'https://zilyo.p.mashape.com/search',
+      qs: { swlatitude: '18.55', swlongitude: '-160.52999999999997' },
+      headers:
+       { provider: 'airbnb',
+         nelongitude: '154.48000000000002',
+         nelatitude: '22.37',
+         isinstantbook: 'true',
+         'x-mashape-key': 'TIjgWU0XSrmsh0zxEqbXqvmwRoytp17KCCtjsnjGAbKORMhWbX' },
+        };
 
-    var params = {
-
-    };
-
-    res.send('respnse with a resource');
+    request(options, (err, response, body) => {
+      if (err) return response.status(400).send(err);
+      res.send(body);
+    });
 });
 
 module.exports = router;
