50eb7d20b8e0ca54350dae50772e0d467a6427b5
march-dave <ysangy@gmail.com>
2016-07-13 10:17:55 -0700

    mouse out event add to marker but not working

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -16,18 +16,30 @@ app.controller('homeCtrl', function($scope, uiGmapGoogleMapApi, $log, Yelp, Crim
      zoom: 14,
      markers: [ ],
      markersEvents: {
-       click: function(marker, eventName, model) {
+       mouseover: function(marker, eventName, model) {
          $scope.map.window.model = model;
          $scope.map.window.show = true;
        }
+
+       ,mouseout: function() {
+         this.show = false;
+       }
+
      },
+
      window: {
        marker: {
        },
        show: false,
+
        closeClick: function() {
          this.show = false;
        },
+
+       mouseout: function() {
+         this.show = false;
+       },
+
        options: {}
      }
    };
@@ -40,30 +52,41 @@ app.controller('homeCtrl', function($scope, uiGmapGoogleMapApi, $log, Yelp, Crim
 
      Yelp.getGuestHouses().then( res => {
 
-         var lati = 0, long = 0, name = '';
+         var lati = 0, long = 0, name = '', rating = '', image_url = '';
          for(var i=0; i<res.businesses.length; i++) {
            lati = res.businesses[i].location.coordinate.latitude;
            long = res.businesses[i].location.coordinate.longitude;
            name = res.businesses[i].name;
+           rating = res.businesses[i].rating;
+           image_url = res.businesses[i].image_url;
+
+          //  phone = res.businesses[i].display_phone;
+
+          //  console.log('res.businesses[i]', res.businesses[i]);
 
            marker = {
               id: Date.now() + 10,
               latitude: lati,
               longitude: long,
               descript: name,
+              rating: 'User Rating: ' + rating,
+              image_url: image_url,
               options: {
               //  icon:"https://cdn0.iconfinder.com/data/icons/my-house-1/512/013-myhouse-32.png"
                icon:"https://cdn0.iconfinder.com/data/icons/free-business-desktop-icons/32/Home.png"
               }
            }
+
+           $log.log('marker', marker);
+
            $scope.map.markers.push(marker);
+
          }
      });
 
-
          CrimeDate.getCrimeList().then( res => {
 
-           var lati = 0, long = 0, descript = '';
+           var lati = 0, long = 0, descript = '', image_url = '';
            for(var i=0; i<50; i++) {
 
              lati = res[i].y;
@@ -75,14 +98,17 @@ app.controller('homeCtrl', function($scope, uiGmapGoogleMapApi, $log, Yelp, Crim
                latitude: lati,
                longitude: long,
                descript: descript,
+               image_url: 'https://cdn4.iconfinder.com/data/icons/proglyphs-miscellaneous/512/Piracy-64.png',
                options: {
-                 // labelContent: "num: " + i + 100,
-                 // labelAnchor: "0 55",
-                 // labelClass: "marker-labels",
+                //  labelContent: "num: " + i + 100,
+                //  labelAnchor: "0 55",
+                //  labelClass: "marker-labels",
                  icon: "https://cdn4.iconfinder.com/data/icons/proglyphs-miscellaneous/512/Piracy-32.png"
                }
              }
              $scope.map.markers.push(marker);
+
+             $log.log('marker', marker);
            }
          });
 
