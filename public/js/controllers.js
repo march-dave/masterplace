a28557f97deaccedaa74f4f5bdc8db17ea9f4e97
march-dave <ysangy@gmail.com>
2016-07-17 14:48:14 -0700

    geo location functin call remove

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -7,42 +7,6 @@ app.controller('homeCtrl', function($scope, uiGmapGoogleMapApi, $log, Yelp, Crim
   $scope.search = function() {
     console.log('$scope.near', $scope.near);
 
-
-
-      //Init a variable and set a value
-      var width = 10;
-
-      //A "normal" function expression.
-      var callback2 = function(){
-          console.log(width);
-      };
-
-      //A function wrapped in an IIFE (Immediately Invoked Function Expression)
-      //Returns an object
-      var callback = (function(width){
-          var othervar = 1;
-          return {
-              call: function() {
-                  console.log(width);
-              }
-          };
-      })(width);
-
-      //Call them both
-      callback.call();//Outputs 10
-      callback2();//Outputs 10
-
-
-      //Change the value of the global/external variable
-      width = 20;
-
-
-      //Call again
-      callback.call();//Outputs 10
-      callback2();//Outputs 20
-
-
-
   }
 
   // uiGmapGoogleMapApi.then(function(maps) {
